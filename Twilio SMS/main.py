d7b97a815f7d18183b28396900142d9d30ea4c7b
SK <sarthakkarandikar03@gmail.com>
2023-04-02 07:57:00 +0000

    Apr 2, 2023, 1:27 PM

--- Twilio SMS/main.py
+++ Twilio SMS/main.py
@@ -4,15 +4,15 @@ from datetime import *
 from time import *
 
 # Your Account Sid and Auth Token from twilio account
-account_sid = "ACa0989588c68a49c8a2751da99be3c2b1"
-auth_token = "b81b3e0f66a7bcff7373113715a40df1"
+account_sid = ""
+auth_token = ""
 # instantiating the Client
 client = Client(account_sid, auth_token)
 
 while ((datetime.now().hour)<=18):
     # sending message
     message = message = client.messages.create(
-        body="E Ojas", from_="+15092535647", to="+918408849900"
+        body="", from_="", to=""
     )
     # printing the sid after success
     print(message.sid)
