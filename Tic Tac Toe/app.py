e8afc4e479802c31e13ad6bc328a7a3dc1f77ca2
SK <sarthakkarandikar03@gmail.com>
2023-11-19 06:03:59 +0000

    Nov 19, 2023, 11:33 AM

--- Tic Tac Toe/app.py
+++ Tic Tac Toe/app.py
@@ -5,7 +5,12 @@ def app() :
     st.title("Tic Tac Toe")
     col_1, col_2 = st.columns(2)
     with col_1 :
-        print_board()
+        move = find_best_move(board, current_player)
+
+        row, col = move
+        board[row][col] = current_player
+
+        print_board(board)
     with col_2 :
         row = int(st.text_input("Enter row number (0 - 2):"))
         col = int(st.text_input("Enter column number (0 - 2): "))
