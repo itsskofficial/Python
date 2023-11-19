4d988c5300ca9b10533dc74431fd179277f27d20
SK <sarthakkarandikar03@gmail.com>
2023-11-19 06:19:09 +0000

    Nov 19, 2023, 11:49 AM

--- Tic Tac Toe/app.py
+++ Tic Tac Toe/app.py
@@ -34,22 +34,7 @@ def app() :
             current_player = 'O' if current_player == 'X' else 'X'
 
         if st.session_state.player == 'X':
-            best_val = -float('inf')
-            best_move = None
-            alpha = -float('inf')
-            beta = float('inf')
-
-            for row in range(3):
-                for col in range(3):
-                    if st.session_state.board[row][col] == ' ':
-                        st.session_state.board[row][col] = 'X'
-                        move_val = minimax(0, False, alpha, beta)
-                        st.session_state.board[row][col] = ' '
-
-                        if move_val > best_val:
-                            best_move = (row, col)
-                            best_val = move_val
-            return best_move
+            
     
         elif st.session_state.player == 'O':
             row = int(input("Enter row (0, 1, 2): "))
