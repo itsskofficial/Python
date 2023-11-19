d1fd27627b307164884f07b797a22b43b12c7383
SK <sarthakkarandikar03@gmail.com>
2023-11-19 07:11:46 +0000

    Nov 19, 2023, 12:41 PM

--- Tic Tac Toe/utils.py
+++ Tic Tac Toe/utils.py
@@ -3,8 +3,7 @@ import streamlit as st
 def print_board():
     for row in st.session_state.board:
         col_1, col_2, col_3 = st.columns(3)
-        with col_1:
-            st.subheader(label = row[0], divider = False)
+        col_1.subheader(label = row[0], divider = False)
         with col_1:
             st.subheader(label = row[0], divider = False)
         with col_1:
