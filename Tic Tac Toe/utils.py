dfee418a5843ece4d824dd2847bbb7576e0ceef6
SK <sarthakkarandikar03@gmail.com>
2023-11-19 07:09:33 +0000

    Nov 19, 2023, 12:39 PM

--- Tic Tac Toe/utils.py
+++ Tic Tac Toe/utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def print_board():
     for row in st.session_state.board:
-        st.subheader(body = "".join(row), divider = False)
+        co
         st.write("")
         st.write("")
 
