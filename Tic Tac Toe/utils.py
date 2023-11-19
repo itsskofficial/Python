10b7db1c5c5d213b7be7ad7515574dcd4fcfda5e
SK <sarthakkarandikar03@gmail.com>
2023-11-19 06:11:14 +0000

    Nov 19, 2023, 11:41 AM

--- Tic Tac Toe/utils.py
+++ Tic Tac Toe/utils.py
@@ -64,25 +64,4 @@ def minimax(depth, is_maximizing, alpha, beta):
         return min_eval
     
 def find_best_move():
-    if player == 'X':
-        best_val = -float('inf')
-        best_move = None
-        alpha = -float('inf')
-        beta = float('inf')
-
-        for row in range(3):
-            for col in range(3):
-                if st.session_state.board[row][col] == ' ':
-                    st.session_state.board[row][col] = 'X'
-                    move_val = minimax(0, False, alpha, beta)
-                    st.session_state.board[row][col] = ' '
-
-                    if move_val > best_val:
-                        best_move = (row, col)
-                        best_val = move_val
-        return best_move
     
-    elif player == 'O':
-        row = int(input("Enter row (0, 1, 2): "))
-        col = int(input("Enter column (0, 1, 2): "))
-        return row, col
