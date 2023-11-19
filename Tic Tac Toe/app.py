439e5447ea5f1c0b9155e438e94c4bf68a50ea0b
SK <sarthakkarandikar03@gmail.com>
2023-11-19 06:28:22 +0000

    Nov 19, 2023, 11:58 AM

--- Tic Tac Toe/app.py
+++ Tic Tac Toe/app.py
@@ -34,7 +34,7 @@ def app() :
         else :
 
             if st.session_state.player == 'X':
-                st.write("Ai")
+                st.write("AI is playing...")
                 move = find_best_move()
                 row, col = move
                 st.session_state.board[row][col] = st.session_state.player
