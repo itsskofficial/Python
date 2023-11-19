46552540138efc5ee3fde56ba7e9a916043ed631
SK <sarthakkarandikar03@gmail.com>
2023-11-19 06:14:03 +0000

    Nov 19, 2023, 11:44 AM

--- Tic Tac Toe/app.py
+++ Tic Tac Toe/app.py
@@ -11,7 +11,7 @@ def app() :
     st.title("Tic Tac Toe")
     col_1, col_2 = st.columns(2)
     with col_1 :
-        move = find_best_move(board, player)
+        move = find_best_move()
         row, col = move
         board[row][col] = player
 
