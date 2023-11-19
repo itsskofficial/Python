f7d926fe89209a186f537f1bc731e2fd7f3e021b
SK <sarthakkarandikar03@gmail.com>
2023-11-19 05:51:40 +0000

    Nov 19, 2023, 11:21 AM

--- Tic Tac Toe/utils.py
+++ Tic Tac Toe/utils.py
@@ -6,25 +6,25 @@ def print_board():
     st.write()
 
 def evaluate():
-    for row in board:
+    for row in st.session_state.board:
         if all(cell == 'X' for cell in row):
             return 1
         if all(cell == 'O' for cell in row):
             return -1
 
     for col in range(3):
-        if all(board[row][col] == 'X' for row in range(3)):
+        if all(st.session_state.board[row][col] == 'X' for row in range(3)):
             return 1
-        if all(board[row][col] == 'O' for row in range(3)):
+        if all(st.session_state.board[row][col] == 'O' for row in range(3)):
             return -1
 
-    if all(board[i][i] == 'X' for i in range(3)) or all(board[i][2 - i] == 'X' for i in range(3)):
+    if all(st.session_state.board[i][i] == 'X' for i in range(3)) or all(st.session_state.board[i][2 - i] == 'X' for i in range(3)):
         return 1
 
-    if all(board[i][i] == 'O' for i in range(3)) or all(board[i][2 - i] == 'O' for i in range(3)):
+    if all(st.session_state.board[i][i] == 'O' for i in range(3)) or all(st.session_state.board[i][2 - i] == 'O' for i in range(3)):
         return -1
 
-    if all(cell != ' ' for row in board for cell in row):
+    if all(cell != ' ' for row in st.session_state.board for cell in row):
         return 0
 
     return None
