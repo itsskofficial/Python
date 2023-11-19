46cf11d4c000dd4cc92454402150073547341fb9
SK <sarthakkarandikar03@gmail.com>
2023-11-19 07:10:44 +0000

    Nov 19, 2023, 12:40 PM

--- Tic Tac Toe/utils.py
+++ Tic Tac Toe/utils.py
@@ -4,7 +4,7 @@ def print_board():
     for row in st.session_state.board:
         col_1, col_2, col_3 = st.columns(3)
         with col_1:
-            st.subheader(label = "AI is playing...", divider = "red")
+            st.subheader(label = row[0], divider = False)
         st.write("")
         st.write("")
 
