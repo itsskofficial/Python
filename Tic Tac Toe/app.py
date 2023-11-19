1af22707a40e466d95ad1805899543c0094a417b
SK <sarthakkarandikar03@gmail.com>
2023-11-19 06:20:00 +0000

    Nov 19, 2023, 11:50 AM

--- Tic Tac Toe/app.py
+++ Tic Tac Toe/app.py
@@ -37,7 +37,7 @@ def app() :
             st.session_state.board[row][col] = st.session_state.player
     
         else:
-            row = int(input("Enter row (0, 1, 2): "))
+            row = int(st.text_input("Enter row (0, 1, 2): "))
             col = int(input("Enter column (0, 1, 2): "))
             return row, col
             
