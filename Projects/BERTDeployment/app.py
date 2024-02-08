aea0596140bdc34979b4e2c6c48512c9da03c675
SK <sarthakkarandikar03@gmail.com>
2024-02-08 15:03:52 +0530

    practice

--- Projects/BERTDeployment/app.py
+++ Projects/BERTDeployment/app.py
@@ -45,12 +45,12 @@ file_id = '1WsXgYeELEsMmrIOOZmfEYJWZbCg2wl-l'
 # Construct the download link
 url = f'https://drive.google.com/uc?id={file_id}'
 
-output = "./bert_model.h5"
+output = "bert_model.h5"
 
 gdown.download(url, output = output, quiet=False)
 
 bert = TFAutoModel.from_pretrained("bert-base-uncased")
-model_path = "./bert_model.h5"
+model_path = "bert_model.h5"
 my_model = tf.keras.models.load_model(model_path, custom_objects={'TFBertMainLayer': bert.bert})
 
 @app.get("/")
